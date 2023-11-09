c73ada5e4dcd591c8fa5043271bf1fd5d2aea28f
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-11-09 21:33:18 +0530

    SignUp Page

--- app/src/main/java/com/original/sense/psit/SignUpPage.kt
+++ app/src/main/java/com/original/sense/psit/SignUpPage.kt
@@ -1,25 +1,35 @@
 package com.original.sense.psit
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.MailOutline
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.CheckCircle
 import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material.icons.outlined.Phone
 import androidx.compose.material.icons.outlined.Place
+import androidx.compose.material.icons.rounded.Check
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -29,12 +39,16 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -83,7 +97,7 @@ fun SignUpPage(navController: NavHostController , context: MainActivity) {
             modifier = Modifier
                 .fillMaxWidth()
                 .wrapContentHeight()
-                .padding(top = 5.dp)
+                .padding(top = 15.dp)
                 .align(CenterHorizontally),
             fontSize = 17.sp,
             color = Color.White,
@@ -106,25 +120,59 @@ fun SignUpPage(navController: NavHostController , context: MainActivity) {
 
 @Composable
 fun AlreadyAccount() {
-    Row (modifier = Modifier.fillMaxWidth()
-        .wrapContentHeight()){
-
+    Row (modifier = Modifier
+        .fillMaxWidth()
+        .wrapContentHeight()
+        .padding(15.dp),
+       horizontalArrangement = Arrangement.Center){
+        Text(text = "Already have an account?",
+            color = Color.White,
+            modifier = Modifier.padding(start = 10.dp),
+            fontSize = 15.sp,
+            fontWeight = FontWeight.Light,
+            )
+        Text(text = "Log In",
+            color = Color.White,
+            modifier = Modifier.padding(start = 10.dp),
+            fontSize = 15.sp,
+            fontWeight = FontWeight.Light,
+        )
     }
 }
 
 @Composable
 fun ContinueButton() {
-    Row (modifier = Modifier.fillMaxWidth()
+    Row (modifier = Modifier
+        .fillMaxWidth()
         .wrapContentHeight()){
 
+        Button(onClick = { /*TODO*/ },
+            modifier = Modifier.fillMaxWidth()
+                .padding(top = 10.dp)){
+            Text(text = "Continue" , color = Color.White,
+                fontSize = 20.sp,
+                modifier = Modifier.padding(8.dp))
+        }
     }
 }
 
 @Composable
 fun Terms() {
-    Row (modifier = Modifier.fillMaxWidth()
-        .wrapContentHeight()){
+    Row (modifier = Modifier
+        .fillMaxWidth()
+        .wrapContentHeight()
+        .padding(10.dp)
+        .padding(top = 10.dp)){
 
+        MyCheckedState()
+
+        Text(text = "I agree to the Terms of Service and Privacy Policy",
+            color = Color.White,
+            modifier = Modifier.padding(start = 10.dp),
+            fontSize = 15.sp,
+            fontWeight = FontWeight.Light)
+        
+        
     }
 }
 
@@ -326,4 +374,35 @@ fun SimpleOutlinedTextFieldRoom() {
         )
 
     )
+}
+
+@Composable
+fun MyCheckedState() {
+    var checkedState by remember { mutableStateOf(false) }
+//    CircularCheckBox(
+//        checked = checkedState,
+//        onCheckedChange = { checkedState = !checkedState }
+//    )
+//
+    Checkbox(checked = checkedState , onCheckedChange = { checkedState = !checkedState })
+}
+
+@Composable
+fun CircularCheckBox(
+    checked: Boolean,
+    modifier: Modifier = Modifier,
+    onCheckedChange: () -> Unit = {}
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .clip(CircleShape)
+            .clickable { onCheckedChange() }
+    ) {
+        Icon(
+            imageVector = if (checked) Icons.Outlined.CheckCircle else Icons.Rounded.Check,
+            contentDescription = "Checkbox",
+            tint = Color.White
+        )
+    }
 }
