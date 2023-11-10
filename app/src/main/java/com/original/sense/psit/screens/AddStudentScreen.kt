14702e4e053ea87436d1c5e279ac10e20c18dd2d
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-11-10 18:34:30 +0530

    Remove and Add students Screens

--- app/src/main/java/com/original/sense/psit/screens/AddStudentScreen.kt
+++ app/src/main/java/com/original/sense/psit/screens/AddStudentScreen.kt
@@ -1,5 +1,7 @@
 package com.original.sense.psit.screens
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -19,66 +21,92 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.original.sense.psit.R
 import com.original.sense.psit.ui.theme.poppins
 
 
 @Composable
 fun AddStudentScreen() {
 
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .background(Color(0xFF3068de)),
-        horizontalAlignment = Alignment.CenterHorizontally){
-        
-        Text(modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = 30.dp , end = 30.dp , top = 30.dp),
-            fontFamily = poppins,
-            text = "Ready to Add Another Student ?",
-            color = Color.White,
-            textAlign = TextAlign.Center,
-            fontSize = 23.sp
-            )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF3068de)) ,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
-        Box (modifier = Modifier
-            .size(250.dp)
-            .background(Color.Red)
-            .padding(top = 20.dp)){
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 30.dp , end = 30.dp , top = 30.dp) ,
+            fontFamily = poppins ,
+            text = "Ready to Add Another Student ?" ,
+            color = Color.White ,
+            textAlign = TextAlign.Center ,
+            fontSize = 23.sp
+        )
+
+        Box(
+            modifier = Modifier
+                .size(250.dp)
+                .padding(top = 20.dp) ,
+            contentAlignment = Alignment.Center
+        ) {
+
+            Image(painter = painterResource(id = R.drawable.tapellipse) , contentDescription = "")
+
+            Text(
+                text = "TAP" , color = Color.White ,
+                fontFamily = poppins
+            )
 
         }
 
-        Row (modifier = Modifier
-            .padding(top = 20.dp)
-            .fillMaxWidth()
-            .height(50.dp)
+        Row(
+            modifier = Modifier
+                .padding(top = 20.dp)
+                .fillMaxWidth()
+                .height(50.dp) ,
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
 
-            ,
-            horizontalArrangement = Arrangement.SpaceAround){
+            Button(
+                modifier = Modifier
+                    .height(50.dp)
+                    .width(125.dp) ,
+                elevation = ButtonDefaults.buttonElevation(4.dp) ,
+                onClick = {
 
-            Button(modifier = Modifier
-                .height(50.dp)
-                .width(125.dp),
-                onClick = { /*TODO*/ },
-                shape = RectangleShape,
+
+                } ,
+                shape = RectangleShape ,
                 colors = ButtonDefaults.buttonColors(Color(0xFFffffff))
-                ) {
-                Text(text = "No", color = Color.Black)
-                
+            ) {
+                Text(text = "No" , color = Color.Black)
+
             }
 
-            Button(modifier = Modifier
-                .height(50.dp)
-                .width(125.dp) ,
-                onClick = { /*TODO*/ },
-                shape = RectangleShape,
+            Button(
+                modifier = Modifier
+                    .height(50.dp)
+                    .width(125.dp) ,
+                elevation = ButtonDefaults.buttonElevation(4.dp) ,
+                onClick = {
+
+
+                } ,
+                shape = RectangleShape ,
                 colors = ButtonDefaults.buttonColors(Color(0xFF00d084))
             ) {
-                Text(text = "Yes", color = Color.White)
+                Text(text = "Yes" , color = Color.White)
 
             }
 
@@ -86,5 +114,5 @@ fun AddStudentScreen() {
         }
 
     }
-    
+
 }
