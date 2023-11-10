80f54dcd7c72d909c84bef7e932d3e4fc4e27a14
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-11-10 15:00:49 +0530

    Main Screen Functionality

--- app/src/main/java/com/original/sense/psit/SplashScreen.kt
+++ app/src/main/java/com/original/sense/psit/SplashScreen.kt
@@ -5,19 +5,15 @@ import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -32,10 +28,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import com.airbnb.lottie.compose.LottieAnimation
-import com.airbnb.lottie.compose.LottieCompositionSpec
-import com.airbnb.lottie.compose.LottieConstants
-import com.airbnb.lottie.compose.rememberLottieComposition
 import kotlinx.coroutines.delay
 
 
--- app/src/main/java/com/original/sense/psit/screens/HomeScreen.kt
+++ app/src/main/java/com/original/sense/psit/screens/HomeScreen.kt
@@ -1,29 +1,112 @@
 package com.original.sense.psit.screens
 
+import android.widget.SearchView
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.original.sense.psit.R
 
 @Composable
 fun HomeScreen(navController: NavController) {
 
 
-    Box (modifier = Modifier
+    Column(modifier = Modifier
         .fillMaxSize()
         .background(brush = GradientBackground())){
 
 
+        val textState = remember {
+            mutableStateOf(TextFieldValue(""))
+        }
+
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp),
+            Arrangement.SpaceBetween) {
+
+            SearchView(state = textState , placeHolder = "Search" , modifier = Modifier)
+
+
+            Row {
+
+                CircularTapButton()
+                Spacer(modifier = Modifier.padding(6.dp))
+                CircularNotificationButton()
+
+            }
+
+
+        }
 
     }
 
 
 
+}
+
+@Composable
+fun CircularNotificationButton() {
+
+    Box (modifier = Modifier.size(50.dp)){
+            Image(modifier = Modifier
+                .clip(CircleShape)
+                .fillMaxSize(),
+                painter = painterResource(id = R.drawable.notifybutton) , contentDescription = "" )
+
+    }
+}
+
+
+@Composable
+fun CircularTapButton() {
+
+    Box (modifier = Modifier.size(50.dp)){
+
+        Image(modifier = Modifier
+            .clip(CircleShape)
+            .fillMaxSize(),
+            painter = painterResource(id = R.drawable.tap) , contentDescription = "" )
+
+    }
 }
 
 @Composable
@@ -41,4 +124,49 @@ fun GradientBackground(): Brush {
         start = Offset.Zero,
         end = Offset(Float.POSITIVE_INFINITY, Float.POSITIVE_INFINITY)
     )
+}
+
+@Composable
+fun ColumnItem(item: String) {
+    Column(modifier = Modifier.padding(10.dp)) {
+        Text(text = item, modifier = Modifier.padding(vertical = 10.dp), fontSize = 22.sp)
+        Divider()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchView(
+    state: MutableState<TextFieldValue> ,
+    placeHolder: String ,
+    modifier: Modifier
+) {
+
+
+        TextField(
+            value = state.value ,
+            onValueChange = { value ->
+                state.value = value
+            } ,
+            modifier
+                .fillMaxWidth(0.6f)
+                .height(50.dp)
+                .clip(RoundedCornerShape(15.dp))
+                .border(2.dp , Color.DarkGray , RoundedCornerShape(15.dp)) ,
+            placeholder = {
+                Text(text = placeHolder,fontSize = 15.sp, color =  Color(0xFF222228))
+            } ,
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = Color.White
+            ) ,
+            leadingIcon = {
+                Icon(imageVector = Icons.Outlined.Search , contentDescription = "Search", tint =    Color(0xFF222228))
+            },
+            maxLines = 1 ,
+            singleLine = true ,
+            textStyle = TextStyle(
+                color =  Color(0xFF222228) , fontSize = 15.sp
+            )
+        )
+
 }
