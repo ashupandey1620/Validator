831f76eeba7df62cad3a3fd6efffe3c92a78c536
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-11-13 09:42:28 +0530

    Edit Profile Pages

--- app/src/main/java/com/original/sense/psit/screens/EditProfileScreen.kt
+++ app/src/main/java/com/original/sense/psit/screens/EditProfileScreen.kt
@@ -10,7 +10,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -19,6 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
@@ -57,6 +62,86 @@ fun EditProfileScreen(navController: NavHostController) {
 
         }
 
+        NameColumn()
+
     }
 
+}
+
+
+@Composable
+fun NameColumn() {
+
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .wrapContentHeight()
+        .padding(20.dp)
+    ) {
+
+        Text(text = "Name",
+            color = Color.White,
+            fontSize = 20.sp,
+            fontFamily = poppins,
+        )
+
+
+
+EditProfileItemMainScreen(mainText = "Ashutosh Pandey") {
+
+}
+
+        
+
+
+
+
+
+    }
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditProfileItemMainScreen(mainText: String, onClick: () -> Unit) {
+
+    Card(onClick = {},
+        colors = CardDefaults.cardColors(Color(0xFF46464b)),
+        modifier = Modifier
+            .fillMaxWidth()
+
+
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 17.dp , horizontal = 14.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+
+
+                Spacer(modifier = Modifier.width(14.dp))
+                Column(
+                    modifier = Modifier
+                ) {
+                    Text(
+                        text = mainText,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFFfffffe),
+                        fontFamily = poppins
+                    )
+                }
+            }
+
+
+            Icon(tint = Color.Gray,
+                contentDescription = "",
+                modifier = Modifier.size(20.dp),
+                painter = painterResource(id = R.drawable.logout)
+            )
+
+        }
+    }
 }
