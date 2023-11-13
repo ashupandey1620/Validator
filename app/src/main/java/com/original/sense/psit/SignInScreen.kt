d97b454a865943a5e4f1d84d440bb83f59ac2a01
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-11-13 17:48:09 +0530

    LogIn And SignuP Page Screen

--- app/src/main/java/com/original/sense/psit/SignInScreen.kt
+++ app/src/main/java/com/original/sense/psit/SignInScreen.kt
@@ -1,10 +1,18 @@
 package com.original.sense.psit
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import com.original.sense.psit.screens.GradientBackground
 
 @Composable
 fun SignInScreen(navController: NavHostController , context: MainActivity) {
 
+    Column(modifier = Modifier.fillMaxSize().background(brush = GradientBackground())) {
+
+    }
 
 }
