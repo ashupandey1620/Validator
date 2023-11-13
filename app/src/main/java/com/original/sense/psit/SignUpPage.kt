b9d18e7b43e7db3b0e719d5c27964e0f2c79864c
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-11-13 18:01:16 +0530

    LogIn And SignuP Page Screen

--- app/src/main/java/com/original/sense/psit/SignUpPage.kt
+++ app/src/main/java/com/original/sense/psit/SignUpPage.kt
@@ -11,19 +11,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.CheckCircle
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.MailOutline
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Phone
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.outlined.CheckCircle
 import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material.icons.outlined.Person
@@ -31,12 +27,13 @@ import androidx.compose.material.icons.outlined.Phone
 import androidx.compose.material.icons.outlined.Place
 import androidx.compose.material.icons.rounded.Check
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -45,21 +42,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
-import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
@@ -72,7 +63,8 @@ fun SignUpPage(navController: NavHostController , context: MainActivity) {
         modifier = Modifier
             .fillMaxSize()
             .background(brush = GradientBackground())
-            .padding(20.dp),
+            .padding(20.dp)
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
@@ -92,7 +84,7 @@ fun SignUpPage(navController: NavHostController , context: MainActivity) {
             modifier = Modifier
                 .fillMaxWidth()
                 .wrapContentHeight()
-                .padding(top = 15.dp,start = 5.dp)
+                .padding(top = 15.dp , start = 5.dp)
                 .align(CenterHorizontally),
             fontSize = 17.sp,
             color = Color.White,
@@ -101,7 +93,9 @@ fun SignUpPage(navController: NavHostController , context: MainActivity) {
 
         )
 
-        Spacer(modifier = Modifier.fillMaxWidth().height(15.dp))
+        Spacer(modifier = Modifier
+            .fillMaxWidth()
+            .height(15.dp))
         SimpleOutlinedTextFieldName()
         SimpleOutlinedTextFieldUsername()
         SimpleOutlinedTextFieldEmail()
@@ -118,6 +112,7 @@ fun SignUpPage(navController: NavHostController , context: MainActivity) {
                 navController.popBackStack()
                 navController.navigate("HomeGraph")
             },
+                colors = ButtonDefaults.buttonColors(Color(0xFF3068de)),
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(top = 10.dp)){
@@ -127,18 +122,19 @@ fun SignUpPage(navController: NavHostController , context: MainActivity) {
             }
         }
 
-        AlreadyAccount()
+        AlreadyAccount(navController)
     }
 
 }
 
 @Composable
-fun AlreadyAccount() {
+fun AlreadyAccount(navController: NavHostController) {
     Row (modifier = Modifier
         .fillMaxWidth()
-        .wrapContentHeight()
-        .padding(15.dp),
-       horizontalArrangement = Arrangement.Center){
+        .width(20.dp)
+        .padding(top = 10.dp),
+       horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically){
         Text(text = "Already have an account?",
             color = Color.White,
             modifier = Modifier.padding(start = 15.dp),
@@ -146,20 +142,20 @@ fun AlreadyAccount() {
             fontWeight = FontWeight.Light,
             fontFamily = poppins,
             )
-        Text(text = "Log In",
-            color = Color.White,
-            modifier = Modifier.padding(start = 10.dp),
-            fontSize = 15.sp,
-            fontWeight = FontWeight.Light,
-            fontFamily = poppins,
-        )
+
+        TextButton(onClick = { navController.navigate("signIn_page") }, modifier = Modifier.padding(start = 10.dp)) {
+            Text(text = "Log In",
+                color = Color(0xFF3068de),
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Light,
+                fontFamily = poppins,
+            )
+        }
+
     }
 }
 
-@Composable
-fun ContinueButton() {
 
-}
 
 @Composable
 fun Terms() {
