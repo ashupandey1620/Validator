b62649b78ab8ab87eb638c1038659812ff44fd18
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-11-21 02:12:23 +0530

    Add Page UI

--- app/src/main/java/com/original/sense/psit/screens/AddScreen.kt
+++ app/src/main/java/com/original/sense/psit/screens/AddScreen.kt
@@ -20,9 +20,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.outlined.List
+import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
@@ -32,21 +36,29 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.original.sense.psit.PersonModel
+import com.original.sense.psit.R
 import com.original.sense.psit.ui.theme.poppins
 
 
@@ -124,9 +136,11 @@ fun AddScreen(navController: NavController) {
 
             SubjectOfDelegation()
 
+            Spacer(modifier = Modifier.padding(10.dp))
+
             Text(text = "Allot Lectures",
                 fontFamily = poppins,
-                fontSize = 20.sp,
+                fontSize = 22.sp,
                 color = Color.White,
                 fontWeight = FontWeight.Medium)
             AllottedLectures()
@@ -153,6 +167,8 @@ fun DelegationAndSuspension() {
             fontSize = 32.sp,
             color = Color.White,
             fontWeight = FontWeight.ExtraBold)
+
+
 
         var selected by remember { mutableStateOf(false) }
         FilterChip(
@@ -205,19 +221,7 @@ fun DescriptionDelegation() {
         horizontalArrangement = Arrangement.SpaceBetween ,
         verticalAlignment = Alignment.CenterVertically
     ) {
-//        FilterChip(
-//            onClick = {  },
-//            colors = FilterChipDefaults.filterChipColors(
-//                containerColor = Color(0xFF1d1e23),
-//                labelColor = Color.White,
-//                selectedContainerColor = Color.Cyan,
-//                selectedLabelColor = Color.Black,
-//                selectedLeadingIconColor = Color.Black
-//            ),
-//            label = { Text("DESN162606") },
-//            selected = selected,
-//            shape = RoundedCornerShape(25.dp)
-        //)
+
 
 
     }
@@ -243,7 +247,7 @@ fun AllottedLectures() {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(vertical = 4.dp)
+                .padding(vertical = 1.dp)
                 .wrapContentHeight() ,
             horizontalArrangement = Arrangement.SpaceBetween ,
             verticalAlignment = Alignment.CenterVertically
@@ -321,7 +325,7 @@ fun AllottedLectures() {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(vertical = 4.dp)
+                .padding(vertical = 1.dp)
                 .wrapContentHeight() ,
             horizontalArrangement = Arrangement.SpaceBetween ,
             verticalAlignment = Alignment.CenterVertically
@@ -399,7 +403,7 @@ fun AllottedLectures() {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(top = 4.dp)
+                .padding(top = 1.dp)
                 .wrapContentHeight() ,
             horizontalArrangement = Arrangement.SpaceBetween ,
             verticalAlignment = Alignment.CenterVertically
@@ -472,7 +476,7 @@ fun AllottedLectures() {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(top=4.dp)
+                .padding(top = 1.dp)
                 .wrapContentHeight() ,
             horizontalArrangement = Arrangement.SpaceBetween ,
             verticalAlignment = Alignment.CenterVertically
@@ -538,10 +542,64 @@ fun AllottedLectures() {
 
 @Composable
 fun SubjectOfDelegation() {
-    Column(modifier = Modifier
-        .size(200.dp)
-        .background(Color.Cyan)) {
 
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .wrapContentHeight()) {
+
+
+
+        Text(text = "Subject Of Delegation:",
+            fontFamily = poppins,
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Medium,
+            color = Color.White
+            )
+
+        val keyboardController = LocalSoftwareKeyboardController.current
+        var text by rememberSaveable { mutableStateOf("") }
+
+        val containerColor = Color(0xFF383838)
+        OutlinedTextField(
+            value = text ,
+            leadingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.titledelegation) ,
+                    contentDescription = "Title" ,
+                    tint = Color(0xFFA7A7A7)
+                )
+            } ,
+            onValueChange = { text = it } ,
+            shape = RoundedCornerShape(30.dp) ,
+
+            placeholder = { Text(text = "Title" , color = Color(0xFFA7A7A7) , fontSize = 16.sp) } ,
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Next ,
+                keyboardType = KeyboardType.Text
+            ) ,
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedTextColor = Color.White ,
+                focusedContainerColor = containerColor ,
+                unfocusedContainerColor = containerColor ,
+                disabledContainerColor = containerColor ,
+                focusedBorderColor = Color.White ,
+                unfocusedBorderColor = Color(0xFF383838) ,
+            ) ,
+            singleLine = true ,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(65.dp)
+                .padding(top = 5.dp) ,
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    keyboardController?.hide()
+
+
+
+                }
+            )
+
+        )
     }
 }
 
