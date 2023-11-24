7b71f20f111788dfb5182f427b7346540fe1807b
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-11-24 06:48:19 +0530

    NFC Enableisation

--- app/src/main/java/com/original/sense/psit/screens/addName.kt
+++ app/src/main/java/com/original/sense/psit/screens/addName.kt
@@ -1,5 +1,9 @@
 package com.original.sense.psit.screens
 
+import android.app.AlertDialog
+import android.content.Intent
+import android.nfc.NfcAdapter
+import android.provider.Settings
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,11 +13,26 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.startActivity
 
 @Composable
 fun addName() {
 
-
+//    val nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+//
+//    if (nfcAdapter == null || !nfcAdapter.isEnabled) {
+//        // NFC is not supported or disabled, show alert
+//        val builder = AlertDialog.Builder(this)
+//        builder.setTitle("NFC Scanner")
+//        builder.setMessage("Please enable NFC scanner to use this feature.")
+//        builder.setPositiveButton("Open Settings") { dialog, which ->
+//            // Open NFC settings
+//            val intent = Intent(Settings.ACTION_NFC_SETTINGS)
+//            startActivity(intent)
+//        }
+//        builder.setNegativeButton("Cancel", null)
+//        builder.create().show()
+//    }
 
 }
 
