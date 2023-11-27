3a9190329cac045ef2743d0197a0529f6886e210
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-11-27 19:13:31 +0530

    Detailed Screen Work

--- app/src/main/java/com/original/sense/psit/screens/detailScreen.kt
+++ app/src/main/java/com/original/sense/psit/screens/detailScreen.kt
@@ -8,17 +8,26 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.original.sense.psit.R
+import com.original.sense.psit.ui.theme.poppins
 
+const val name = "Ashutosh Pandey"
 @Composable
 fun detailScreen(navController: NavController) {
 
@@ -31,10 +40,7 @@ fun detailScreen(navController: NavController) {
             Arrangement.SpaceBetween
         ) {
 
-            SearchView(
-                placeHolder = "Search" ,
-                modifier = Modifier
-            )
+            FrontLobe(modifier = Modifier)
 
             Row {
 
@@ -58,7 +64,65 @@ fun detailScreen(navController: NavController) {
                 CircularNotificationButton(navController)
             }
         }
+
+
+
+
     }
 
+
+}
+
+@Composable
+fun FrontLobe( modifier: Modifier) {
+
+    Row ( modifier
+        .fillMaxWidth(0.7f)
+        .height(50.dp)){
+
+        Box(modifier = Modifier.size(50.dp)) {
+
+            Card(modifier = Modifier.fillMaxSize() ,
+                shape = CircleShape,
+                colors = CardDefaults.cardColors(Color(0xFF383841))) {
+
+
+                Column(
+                    modifier = Modifier.fillMaxSize() ,
+                    Arrangement.Center ,
+                    Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "AS" ,
+                        color = Color.White ,
+                        fontSize = 20.sp ,
+                        fontFamily = poppins ,
+                    )
+                }
+            }
+
+        }
+
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 5.dp),
+            verticalArrangement = Arrangement.SpaceBetween) {
+
+            Text(text = name,
+                color = Color.White,
+                fontSize = 16.sp,
+                fontFamily = poppins ,
+            )
+
+            Text(text = "2101641530046",
+                color = Color.White,
+                fontSize = 14.sp,
+                fontFamily = poppins ,
+            )
+
+        }
+
+
+    }
 
 }
