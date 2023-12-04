9a0918a8a82e5af23ad77ad291c4b3e99b058b9a
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-12-04 10:18:45 +0530

    Light mode Switch UI

--- app/src/main/java/com/original/sense/psit/screens/detailScreen.kt
+++ app/src/main/java/com/original/sense/psit/screens/detailScreen.kt
@@ -1,7 +1,9 @@
 package com.original.sense.psit.screens
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,22 +14,33 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.original.sense.psit.R
+import com.original.sense.psit.composable.AddStudentScreen
 import com.original.sense.psit.composable.GradientBackground
 import com.original.sense.psit.ui.theme.poppins
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 const val name = "Ashutosh Pandey"
 @Composable
@@ -84,6 +97,63 @@ fun detailScreen(navController: NavController) {
 
 @Composable
 fun DateAndCalendar() {
+    val context = LocalContext.current.applicationContext
+    val calendar = Calendar.getInstance()
+    val year = calendar.get(Calendar.YEAR)
+    val month = calendar.get(Calendar.MONTH) // 0-based index
+
+    val simpleDateFormat = SimpleDateFormat("MMMM, YYYY", Locale.ENGLISH)
+    val formattedDate = simpleDateFormat.format(calendar.time)
+
+
+
+
+    Toast.makeText(context,"month ${year}",Toast.LENGTH_SHORT).show()
+
+    Toast.makeText(context,"month ${month}",Toast.LENGTH_SHORT).show()
+
+    var show by remember { mutableStateOf(false) }
+
+
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .wrapContentHeight()){
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Text(text = formattedDate,
+                color = Color.White,
+                fontSize = 20.sp,
+                fontFamily = poppins ,
+            )
+
+            Icon(tint = Color.Gray,
+                contentDescription = "",
+                modifier = Modifier
+                    .size(20.dp)
+                    .clickable {
+                               show = !show
+                    },
+                painter = painterResource(id = R.drawable.ic_right_arrow)
+            )
+
+        }
+
+
+        if(show){
+            AddStudentScreen()
+        }
+
+
+
+    }
+
+
 
 }
 
