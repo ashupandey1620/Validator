1bbf595adaa848b48e1c72bc8206d05d6d317a7c
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-12-11 22:41:01 +0530

    Get Student Api

--- app/src/main/java/com/original/sense/psit/Repository/PsitRepository.kt
+++ app/src/main/java/com/original/sense/psit/Repository/PsitRepository.kt
@@ -3,10 +3,12 @@ package com.original.sense.psit.Repository
 import android.util.Log
 import com.original.sense.psit.API.PsitApi
 import com.original.sense.psit.model.PostModel.ChangePasswordPost
+import com.original.sense.psit.model.PostModel.GetPwdPost
 import com.original.sense.psit.model.PostModel.GetStudentPost
 import com.original.sense.psit.model.PostModel.LoginPost
 import com.original.sense.psit.model.PostModel.TempRegisterPost
 import com.original.sense.psit.model.ResponseModel.ChangePasswordResponse
+import com.original.sense.psit.model.ResponseModel.GetPwdResponse
 import com.original.sense.psit.model.ResponseModel.GetStudentResponse
 import com.original.sense.psit.model.ResponseModel.LoginResponse
 import com.original.sense.psit.model.ResponseModel.TempRegister
@@ -70,6 +72,22 @@ class PsitRepository @Inject constructor(private val psitApi : PsitApi){
     suspend fun getStudent(accessToken:String , getStudentPost: GetStudentPost): GetStudentResponse? {
         return try {
             val response = psitApi.getStudent("Bearer $accessToken",getStudentPost)
+
+            if (response.isSuccessful) {
+                response.body()
+            } else {
+                // Handle unsuccessful response (maybe return null or throw an exception)
+                null
+            }
+        } catch (e: Exception) {
+            // Handle exceptions here
+            null
+        }
+    }
+
+    suspend fun getPwdChip(accessToken:String , getPwdPost: GetPwdPost): GetPwdResponse? {
+        return try {
+            val response = psitApi.getChipPwd("Bearer $accessToken",getPwdPost)
 
             if (response.isSuccessful) {
                 response.body()
